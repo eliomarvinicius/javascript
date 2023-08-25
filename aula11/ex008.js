652841777f376e7a685969980cca529a0bf8ad61
Eliomar-Vinicius7 <138163810+Eliomar-Vinicius7@users.noreply.github.com>
2023-08-25 14:01:19 -0300

    aula 11 de javascript

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 66
+var limite = 100
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel >=60){ //condição simples
+    console.log(`Voçê ultrapassou o limite de velocidade. Uma multa será emitida!`)
+}    else {
+    console.log(`Voçê esta dentro do limite de velocidade. Dirija sempre usando cinto de segurança!`)
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Vivendo no ${país}`)
+if (país == 'Brasil'){
+    console.log(`Voçê é Brasileiro!`)
+} else {
+    console.log(`Voçê é Estrangeiro!`)
+}

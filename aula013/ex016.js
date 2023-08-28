26e90f64a70ac641199bd9dc6a109f04a0082fd1
Eliomar-Vinicius7 <138163810+Eliomar-Vinicius7@users.noreply.github.com>
2023-08-28 20:21:54 -0300

    aula 13 e 14 finalizada

---
+++ aula013/ex016.js
@@ -0,0 +1,25 @@
+var c = 1
+do { 
+    console.log(`Pass ${c}: Tudo Bem?`)
+    c++ //c = c + 1
+} while (c <= 6) 
+
+
+
+
+/*
+Repetição com teste no início
+var c = 1
+while (c <= 6) { 
+    console.log(`Pass ${c}: Tudo Bem?`)
+    c++ //c = c + 1
+}
+*/
+
+/*
+console.log(`Tudo Bem?`)
+console.log(`Tudo Bem?`)
+console.log(`Tudo Bem?`)
+console.log(`Tudo Bem?`)
+console.log(`Tudo Bem?`)
+*/

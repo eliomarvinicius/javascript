8ff3dbaabeb5ce49acc29caeb81b66f7f33cd731
Eliomar-Vinicius7 <138163810+Eliomar-Vinicius7@users.noreply.github.com>
2023-08-29 17:40:09 -0300

    aula014ex017 finalizado

--- aula014ex/ex017/script.js
+++ aula014ex/ex017/script.js
@@ -1,4 +1,19 @@
-var agora = new Date()
-hora = agora.getHours
-
-txt = window.docu
+function tabuada() {
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert(`Por favor insira um número!`)
+    } else {
+        var n = Number(num.value)
+        var c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            var item = document.createElement('Option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
+    
